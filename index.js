780cf089e8ebfe567f3eb2b4d2e12083668d06c0
chayon1001 <baust.cse.200201001@gmail.com>
2024-12-08 15:12:48 +0600

    added the my watchlist funtionaliyty show the revriw details and also user can manage

--- index.js
+++ index.js
@@ -14,116 +14,141 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.crj7d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
 });
 
 async function run() {
-  try {
-    
-    await client.connect();
+    try {
 
-    // Database and Collections
-    const gameCollection = client.db('gameDB').collection('game');
-    const watchlistCollection = client.db('gameDB').collection('watchlist');
+        await client.connect();
+
+        // Database and Collections
+        const gameCollection = client.db('gameDB').collection('game');
+        const watchlistCollection = client.db('gameDB').collection('watchlist');
 
 
-    app.get('/userReviews', async (req, res) => {
-        const { email } = req.query;
-        if (!email) {
-            return res.status(400).send({ error: "User email is required" });
-        }
-    
-        const reviews = await gameCollection.find({ userEmail: email }).toArray();
-        res.send(reviews);
+        app.get('/userReviews', async (req, res) => {
+            const { email } = req.query;
+            if (!email) {
+                return res.status(400).send({ error: "User email is required" });
+            }
+
+            const reviews = await gameCollection.find({ userEmail: email }).toArray();
+            res.send(reviews);
+        });
+
+
+        app.get('/review/:id', async (req, res) => {
+            const { id } = req.params;
+            const query = { _id: new ObjectId(id) };
+            const review = await gameCollection.findOne(query);
+            res.send(review);
+        });
+
+        // Fetch all reviews
+        app.get('/review', async (req, res) => {
+            const cursor = gameCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
+
+
+      // Get the user's watchlist by email
+      app.get('/myWatchlist/:id', async (req, res) => {
+            const {id} = req.params;
+            const query = { _id: new ObjectId(id) };
+            const review = await watchlistCollection.findOne(query);
+            res.send(review);
     });
 
 
-    app.get('/review/:id', async (req, res) => {
-        const { id } = req.params;
-        const query = { _id: new ObjectId(id) };
-        const review = await gameCollection.findOne(query);
-        res.send(review);
-      });
+        // Add a new review
+        app.post('/addReview', async (req, res) => {
+            const game = req.body;
+            console.log('New Game Review:', game);
 
-    // Fetch all reviews
-    app.get('/review', async (req, res) => {
-      const cursor = gameCollection.find();
-      const result = await cursor.toArray();
-      res.send(result);
-    });
-
-    // Add a new review
-    app.post('/addReview', async (req, res) => {
-      const game = req.body;
-      console.log('New Game Review:', game);
-
-      const result = await gameCollection.insertOne(game);
-      res.send(result);
-    });
-
-   
-    
-
-    // Add review to watchlist
-    app.post('/watchlist', async (req, res) => {
-      const { review, userEmail, userName } = req.body;
-
-      
-
-      const watchlistItem = {
-        ...review,
-        userEmail,
-        userName
-       
-      };
-
-      const result = await watchlistCollection.insertOne(watchlistItem);
-      res.send(result);
-    });
-
-   
-
-    app.put('/review/:id', async (req, res) => {
-        const { id } = req.params;
-        const updatedReview = req.body;
-    
-       
-        delete updatedReview._id;
-    
-        const result = await gameCollection.updateOne(
-            { _id: new ObjectId(id) },
-            { $set: updatedReview }
-        );
-    
-        res.send(result);
-    });
+            const result = await gameCollection.insertOne(game);
+            res.send(result);
+        });
 
 
-    app.delete('/review/:id', async (req, res) => {
-        const id = req.params.id;
-        const result = await gameCollection.deleteOne({ _id: new ObjectId(id) });
-        res.send(result);
-    });
 
-    // Confirm MongoDB connection
-    await client.db('admin').command({ ping: 1 });
-    console.log('Successfully connected to MongoDB');
-  } finally {
-    // Keep the connection open for the server
-  }
+
+        // Add review to watchlist
+        app.post('/watchlist', async (req, res) => {
+            const { review, userEmail, userName } = req.body;
+
+
+
+            const watchlistItem = {
+                ...review,
+                userEmail,
+                userName
+
+            };
+
+            const result = await watchlistCollection.insertOne(watchlistItem);
+            res.send(result);
+        });
+
+
+
+        app.put('/review/:id', async (req, res) => {
+            const { id } = req.params;
+            const updatedReview = req.body;
+
+
+            delete updatedReview._id;
+
+            const result = await gameCollection.updateOne(
+                { _id: new ObjectId(id) },
+                { $set: updatedReview }
+            );
+
+            res.send(result);
+        });
+
+
+        app.delete('/review/:id', async (req, res) => {
+            const id = req.params.id;
+            const result = await gameCollection.deleteOne({ _id: new ObjectId(id) });
+            res.send(result);
+        });
+
+
+        // Delete a game from the watchlist
+
+
+        app.delete('/myWatchlist/:id', async (req, res) => {
+            const id = req.params.id;
+            const result = await watchlistCollection.deleteOne({ _id: new ObjectId(id) });
+            res.send(result);
+           
+        });
+
+
+
+
+        // Confirm MongoDB connection
+        await client.db('admin').command({ ping: 1 });
+        console.log('Successfully connected to MongoDB');
+    } finally {
+        // Keep the connection open for the server
+    }
 }
 run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-  res.send('Game Review Server is running');
+    res.send('Game Review Server is running');
 });
 
 
 app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
+    console.log(`Server is running on port: ${port}`);
 });
